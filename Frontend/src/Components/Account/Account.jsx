5bf264cc42548f793c08254e10321ad5e12f88d1
Jigme Nidup <jigmenidup8102003@gmail.com>
2025-03-23 22:32:04 +0530

    small change

--- Frontend/src/Components/Account/Account.jsx
+++ Frontend/src/Components/Account/Account.jsx
@@ -4,7 +4,7 @@ const Account = () => {
   return (
     <div>
       <h1>Account Page</h1>
-      <p>Manage your account settings here.</p>
+      <p>Manage your account settings here!</p>
     </div>
   );
 };
